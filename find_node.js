7edeec05ee4d85b0a3bb6e33669f7d6d743a946b
Andre Oliveira <andre.oliverr678@gmail.com>
2026-05-13 16:01:33 -0300

    commit

--- find_node.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('c:/Users/adasilva/Downloads/CRM/wf_barbearia.json', 'utf16le');
-if (content.charCodeAt(0) === 0xFEFF) {
-    content = content.slice(1);
-}
-const wf = JSON.parse(content);
-const node = wf.nodes.find(n => n.name === 'listaMensagens');
-console.log(JSON.stringify(node, null, 2));
--- get_node_params.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('c:/Users/adasilva/Downloads/CRM/check_main.json', 'utf16le');
-if (content.charCodeAt(0) === 0xFEFF) content = content.slice(1);
-const wf = JSON.parse(content);
-const node = wf.nodes.find(n => n.name.startsWith('Andr') && n.name.includes('Atendente'));
-console.log(JSON.stringify(node.parameters, null, 2));
